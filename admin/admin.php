34905e44c211c4636c5adb4d6a81224cb67bec2c
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-01-07 02:42:07 +0100

    -

--- admin/admin.php
+++ admin/admin.php
@@ -78,7 +78,6 @@ $logs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="./../assets/css/admin.css"> -->
     <title>Admin Panel</title>
 </head>
 
