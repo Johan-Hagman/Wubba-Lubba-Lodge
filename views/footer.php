ce2a22442a98721ba3f542409038b1e264a43191
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-01-09 06:37:10 +0100

    -

--- views/footer.php
+++ views/footer.php
@@ -4,7 +4,7 @@
         <p>Get Schwifty! Your stay is guaranteed to be *out of this world*!</p>
         <nav>
             <a href="#terms">Terms of Service</a> |
-            <a href="#privacy">Privacy Policy</a> |
+            <a href="#privacy">Privacy Policy</a>
         </nav>
     </div>
 </footer>
