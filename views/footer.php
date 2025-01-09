3a5162fe41e8fd41a2121ee76dbebaaf4ef3569f
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-01-09 18:16:52 +0100

    moved admin panel to footer

--- views/footer.php
+++ views/footer.php
@@ -2,9 +2,8 @@
     <div class="footer-content">
         <p>&copy; <?php echo date('Y'); ?> Wubba Lubba Lodge. All dimensions reserved.</p>
         <p>Get Schwifty! Your stay is guaranteed to be *out of this world*!</p>
-        <nav>
-            <a href="#terms">Terms of Service</a> |
-            <a href="#privacy">Privacy Policy</a>
-        </nav>
+
+        <a href="./admin/login.php" class="cta-link">Rick's Hub</a>
+
     </div>
 </footer>
