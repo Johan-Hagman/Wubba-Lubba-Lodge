8ad80de97132d6e10dc583953b809b3d850798f9
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-01-09 05:42:42 +0100

    -

--- views/footer.php
+++ views/footer.php
@@ -3,8 +3,8 @@
         <p>&copy; <?php echo date('Y'); ?> Wubba Lubba Lodge. All dimensions reserved.</p>
         <p>Get Schwifty! Your stay is guaranteed to be *out of this world*!</p>
         <nav>
-            <a href="/terms">Terms of Service</a> |
-            <a href="/privacy">Privacy Policy</a> |
+            <a href="#terms">Terms of Service</a> |
+            <a href="#privacy">Privacy Policy</a> |
         </nav>
     </div>
 </footer>
